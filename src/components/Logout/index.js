a4a6eae455df9a7af91fdfec230522722fac4914
Krishnavamsifsd <Krishnavamsifsd@gmail.com>
2023-04-05 13:48:00 +0530

    first commit

---
+++ src/components/Logout/index.js
@@ -0,0 +1,10 @@
+const Logout = () => {
+    document.title = "Boxigo App | Logout"
+    return (
+    <div>
+        <h1>Logout</h1>
+        <p>Logout is under Integration</p>
+    </div>
+)}
+
+export default Logout
